ff2ec98c4a51a7d47b283b69dbc83ac2a1bfe037
siddharth200119 <siddramnani@gmail.com>
2024-02-27 11:52:04 +0530

    started developing web app

---
+++ Web App/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require('path');
+const app = express()
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views'));
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3069;
+}
+
+app.listen(port, function (){
+    console.log("Mobile App started");
+})
+
+app.get("/", function(req, res){
+    res.render("home");
+})
