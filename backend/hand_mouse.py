86ddc3706bf979b1acaeacb809861d7539625b0a
siddharth200119 <siddramnani@gmail.com>
2024-03-01 12:26:40 +0530

    minor doc fix in BGR2GREY (dosent work with mediapipe)

--- backend/hand_mouse.py
+++ backend/hand_mouse.py
@@ -10,20 +10,17 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_c
 screen_width, screen_height = pyautogui.size()
 
 def hand_mouse(frame):
-    # Convert the image from BGR to GREYSCALE for faster processing
-    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     #process to detect hands in frame
-    results = hands.process(grey_frame)
+    results = hands.process(rgb_frame)
 
     #check if hands are detected
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             palm = (hand_landmarks.landmark[0].x, hand_landmarks.landmark[0].y)
-            print("palm")
-            print(palm)
             try:
-                pyautogui.moveTo(palm[0]*screen_width, palm[1]*screen_height)
+                pyautogui.dragTo(palm[0]*screen_width, palm[1]*screen_height)
             except:
                 print("failed")
             
